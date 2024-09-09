5d508e77e47d08c3fdb68b8fa894bdb1581c32f2
aier <aierjjwu8@gmail.com>
2024-09-09 17:57:22 -0400

    Changed number of workspaces from 4 to 3

--- aier/home/dconf/gnomeDesktopWMPreferences.nix
+++ aier/home/dconf/gnomeDesktopWMPreferences.nix
@@ -8,7 +8,7 @@ with lib.hm.gvariant;
     "org/gnome/desktop/wm/preferences" = {
       action-middle-click-titlebar = "minimize";
       button-layout = "appmenu:minimize,maximize,close";
-      num-workspaces = 4;
+      num-workspaces = 3;
     };
 
   };
