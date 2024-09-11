7813c0da12c2331e89be31f80f2589c070610aed
aier <aierjjwu8@gmail.com>
2024-09-11 19:24:21 -0400

    removed sassc and unused extensions

--- aier/home/homeApps.nix
+++ aier/home/homeApps.nix
@@ -17,16 +17,11 @@
     darktable # Lightroom Linux Alt
     davinci-resolve # Premiere Pro Linux Alt
     inkscape # Photoshop/Paint
-    
-    # programs
-    sassc
 
     
     # Gnome Extensions
     gnomeExtensions.appindicator
-    # gnomeExtensions.auto-move-windows
     gnomeExtensions.clipboard-indicator
-    # gnomeExtensions.gtk4-desktop-icons-ng-ding
     gnomeExtensions.dash-to-dock
     gnomeExtensions.disable-unredirect-fullscreen-windows
     gnomeExtensions.hide-top-bar
