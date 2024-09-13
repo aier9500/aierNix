6f9c159fe77c67bb344d2b8c7cac2b22c688d896
aier <aierjjwu8@gmail.com>
2024-09-13 19:43:17 -0400

    added ubuntu-sans font

--- aier/home/homeConfigs.nix
+++ aier/home/homeConfigs.nix
@@ -12,6 +12,9 @@
 
   home.file = {
     
+    # fonts
+    ".fonts/ubuntuSans".source = "${pkgs.ubuntu-sans}/share/fonts/truetype/ubuntu-sans";
+    
     # .icons
     ".icons/latteLightCursors".source = "${pkgs.catppuccin-cursors.latteLight}/share/icons/catppuccin-latte-light-cursors";
 
