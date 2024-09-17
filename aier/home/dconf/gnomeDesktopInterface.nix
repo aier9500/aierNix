bf61b0e96cf9add22fa29f0c86bdbd47a42b43f4
aier <aierjjwu8@gmail.com>
2024-09-17 19:41:27 -0400

    Changed default font

--- aier/home/dconf/gnomeDesktopInterface.nix
+++ aier/home/dconf/gnomeDesktopInterface.nix
@@ -8,11 +8,11 @@ with lib.hm.gvariant;
     "org/gnome/desktop/interface" = {
       color-scheme = "default";
       cursor-theme = "latteLightCursors";
-      document-font-name = "Cantarell 11";
+      document-font-name = "System-ui 11";
       enable-hot-corners = false;
       font-antialiasing = "rgba";
       font-hinting = "full";
-      font-name = "Cantarell 12";
+      font-name = "System-ui 12";
       gtk-theme = "adwGtk3";
       monospace-font-name = "Monospace 11";
       show-battery-percentage = true;
