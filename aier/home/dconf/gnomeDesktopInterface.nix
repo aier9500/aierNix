5fba8d0ca730106cd48774182f4ae7894e4dfa57
aier <aierjjwu8@gmail.com>
2024-09-18 08:49:02 -0400

    added font, theme, and cursor config

--- aier/home/dconf/gnomeDesktopInterface.nix
+++ aier/home/dconf/gnomeDesktopInterface.nix
@@ -7,14 +7,14 @@ with lib.hm.gvariant;
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       # color-scheme = "default";
-      # cursor-theme = "latteLightCursors";
-      # document-font-name = "System-ui 11";
+      cursor-theme = "LatteLightCursors";
+      document-font-name = "IBM Plex Serif weight=450 11";
       enable-hot-corners = false;
       font-antialiasing = "rgba";
       font-hinting = "full";
-      # font-name = "System-ui 12";
-      # gtk-theme = "adwGtk3";
-      # monospace-font-name = "Monospace 11";
+      font-name = "IBM Plex Sans 11";
+      gtk-theme = "AdwGtk3";
+      monospace-font-name = "IBM Plex Mono 10";
       show-battery-percentage = true;
     };
 
