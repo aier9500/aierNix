a922c6a651a6807190f52a124a6fcccf7b75c5e6
aier <aierjjwu8@gmail.com>
2024-09-19 11:19:38 -0400

    Fixed interface font runtime bug

--- aier/home/dconf/gnomeDesktopInterface.nix
+++ aier/home/dconf/gnomeDesktopInterface.nix
@@ -12,7 +12,7 @@ with lib.hm.gvariant;
       enable-hot-corners = false;
       font-antialiasing = "rgba";
       font-hinting = "full";
-      font-name = "Noto Sans CJK SC 11 @wght=400"; # Sans/Interface
+      font-name = "IBM Plex Sans 11"; # Sans/Interface
       gtk-theme = "AdwGtk3";
       monospace-font-name = "IBM Plex Mono 10"; # Mono
       show-battery-percentage = true;
