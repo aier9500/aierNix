bde3fbfe703ad5b769e75e64a5a9ab78b6ac6c48
aier <aierjjwu8@gmail.com>
2024-09-25 23:36:06 -0400

    Removed error

--- aier/configuration/systemConfigs.nix
+++ aier/configuration/systemConfigs.nix
@@ -22,7 +22,7 @@
   networking = {
     hostName = "aierNixOS"; # Define your hostname.
     hostId = "9edbe06b"; 
-    wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     # Enable networking
     networkmanager.enable = true;
   };
