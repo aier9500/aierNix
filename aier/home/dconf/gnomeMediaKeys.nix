98a4ccb303e695d2b59999b48b9a0c500e50d644
aier <aierjjwu8@gmail.com>
2024-09-29 12:18:33 -0400

    Fixed custom keybindings

--- aier/home/dconf/gnomeMediaKeys.nix
+++ aier/home/dconf/gnomeMediaKeys.nix
@@ -8,7 +8,7 @@ with lib.hm.gvariant;
     "org/gnome/settings-daemon/plugins/media-keys" = {
       calculator = [ "<Super>c" ];
       control-center = [ "<Super>i" ];
-      # custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/" ];
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/" ];
       help = [];
       home = [ "<Super>e" ];
       magnifier = [ "<Super>z" ];
@@ -20,31 +20,31 @@ with lib.hm.gvariant;
       www = [ "<Super>b" ];
     };
 
-    "custom-keybindings/custom0" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control><Super>Delete";
       command = "gnome-session-quit";
       name = "gnome-session-quit";
     };
 
-    "custom-keybindings/custom1" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       binding = "<Shift><Super>r";
       command = "gnome-terminal";
       name = "gnome-terminal";
     };
 
-    "custom-keybindings/custom2" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
       binding = "<Super>t";
       command = "gnome-text-editor";
       name = "gnome-text-editor";
     };
 
-    "custom-keybindings/custom3" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
       binding = "<Shift><Control>Escape";
       command = "resources";
       name = "resources";
     };
 
-    "custom-keybindings/custom4" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
       binding = "<Shift><Control><Alt>r";
       command = "obs";
       name = "obs";
