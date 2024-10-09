8cb2dc95e864cf90467c8196ff241eaf12d984c8
aier <aierjjwu8@gmail.com>
2024-10-09 14:45:30 -0400

    Renamed extension dconf file

--- aier/home/homeDconf.nix
+++ aier/home/homeDconf.nix
@@ -13,7 +13,7 @@ with lib.hm.gvariant;
     ./dconf/gnomeExtensionsDashToDock.nix
     ./dconf/gnomeExtensionsHidetopbar.nix
     ./dconf/gnomeExtensionsPaperWM.nix
-    ./dconf/gnomeExtensionsRoundedCornersReborn.nix
+    ./dconf/gnomeExtensionsRoundedWindowCornersReborn.nix
     ./dconf/gnomeExtensionsUnite.nix
     ./dconf/gnomeMediaKeys.nix
     ./dconf/gnomeMutter.nix
