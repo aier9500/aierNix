25eb9c37f938468ea288dfad919584ad18d04261
aier <aierjjwu8@gmail.com>
2024-10-18 22:20:33 -0400

    added tetrio-desktop

--- aier/home/homeApps.nix
+++ aier/home/homeApps.nix
@@ -5,7 +5,6 @@
   home.packages = (with pkgs; [    # User Apps
     audacity
     blender
-    chromium
     darktable
     davinci-resolve
     dconf2nix
@@ -20,6 +19,7 @@
     libreoffice
     osu-lazer
     steam
+    tetrio-desktop
     texliveFull
     vesktop
     vial
