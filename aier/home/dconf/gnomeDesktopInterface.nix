daf71d216904370faaebfa17f9139c6d9f1f1ef5
aier <aierjjwu8@gmail.com>
2024-10-21 20:17:51 -0400

    Declaring shell theme

--- aier/home/dconf/gnomeDesktopInterface.nix
+++ aier/home/dconf/gnomeDesktopInterface.nix
@@ -4,21 +4,24 @@
 with lib.hm.gvariant;
 
 {
-  # The shell theme is not set declaratively
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       clock-format = "24h"; 
       color-scheme = "default"; # Prefer dark or light
       cursor-theme = "LatteLightCursors";
-      document-font-name = "Noto Serif CJK TC 11 @wght=400"; # Serif/Document
+      document-font-name = "Noto Serif CJK TC 11 @wght=400"; # Serif/Document font
       enable-hot-corners = false;
       font-antialiasing = "rgba";
       font-hinting = "full";
-      font-name = "IBM Plex Sans 11"; # Sans/Interface
-      # gtk-theme = "AdwGtk3";
-      monospace-font-name = "IBM Plex Mono 10"; # Mono
+      font-name = "IBM Plex Sans 11"; # Sans/Interface/Backup font
+      # gtk-theme = "AdwGtk3"; # Legacy application theming
+      monospace-font-name = "IBM Plex Mono 10"; # Mono font
       show-battery-percentage = true;
       text-scaling-factor = 0.87; 
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "MarbleBlueLight";
     };
 
   };
