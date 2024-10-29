bbb4ba73d395e1478504b1e5617b566efaa47179
aier <aierjjwu8@gmail.com>
2024-10-29 19:28:41 -0400

    Renaming for consistency

--- default/home/homeDconf.nix
+++ default/home/homeDconf.nix
@@ -5,20 +5,20 @@ with lib.hm.gvariant;
 {
   
   imports = [
-    ./dconf/gnomeDesktopInterface.nix
-    ./dconf/gnomeDesktopWMKeybindings.nix
-    ./dconf/gnomeDesktopWMPreferences.nix
-    ./dconf/gnomeExtensionsBlurMyShell.nix
-    ./dconf/gnomeExtensionsClipboardManager.nix
-    ./dconf/gnomeExtensionsDashToDock.nix
-    ./dconf/gnomeExtensionsHidetopbar.nix
-    ./dconf/gnomeExtensionsPaperWM.nix
-    ./dconf/gnomeExtensionsQuickSettingsAudioPanel.nix
-    ./dconf/gnomeExtensionsRoundedWindowCornersReborn.nix
-    ./dconf/gnomeExtensionsUnite.nix
-    ./dconf/gnomeMediaKeys.nix
-    ./dconf/gnomeMutter.nix
-    ./dconf/gnomeShellKeybindings.nix
-    ./dconf/gnomeTouchpad.nix
+    ./homeDconf/gnomeDesktopInterface.nix
+    ./homeDconf/gnomeDesktopWMKeybindings.nix
+    ./homeDconf/gnomeDesktopWMPreferences.nix
+    ./homeDconf/gnomeExtensionsBlurMyShell.nix
+    ./homeDconf/gnomeExtensionsClipboardManager.nix
+    ./homeDconf/gnomeExtensionsDashToDock.nix
+    ./homeDconf/gnomeExtensionsHidetopbar.nix
+    ./homeDconf/gnomeExtensionsPaperWM.nix
+    ./homeDconf/gnomeExtensionsQuickSettingsAudioPanel.nix
+    ./homeDconf/gnomeExtensionsRoundedWindowCornersReborn.nix
+    ./homeDconf/gnomeExtensionsUnite.nix
+    ./homeDconf/gnomeMediaKeys.nix
+    ./homeDconf/gnomeMutter.nix
+    ./homeDconf/gnomeShellKeybindings.nix
+    ./homeDconf/gnomeTouchpad.nix
   ];
 }
