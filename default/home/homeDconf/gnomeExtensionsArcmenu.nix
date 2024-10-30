b93ae78ca846d5740707e3975fe93c96e8cfd5b7
aier <aierjjwu8@gmail.com>
2024-10-30 19:03:33 -0400

    + more settings to arcmenu

--- default/home/homeDconf/gnomeExtensionsArcmenu.nix
+++ default/home/homeDconf/gnomeExtensionsArcmenu.nix
@@ -7,9 +7,13 @@ with lib.hm.gvariant;
   dconf.settings = {
     "org/gnome/shell/extensions/arcmenu" = {
       arcmenu-hotkey = [ "<Alt>space" ];
+      highlight-result-terms = true; 
       menu-button-position-offset = 1;
       menu-layout = "Runner";
+      search-provider-open-windows = true; 
+      search-provider-recent-files = true;
       show-activities-button = true;
+      show-search-result-details = true;
     };
 
   };
