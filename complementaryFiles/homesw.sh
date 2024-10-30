ff1cbd7b790dd8a24adb77d139aabe9c7566edbc
aier <aierjjwu8@gmail.com>
2024-10-29 20:19:30 -0400

    + Scripts to run rebuild commands

---
+++ complementaryFiles/homesw.sh
@@ -0,0 +1,8 @@
+cd ~/.dotfiles/aierNix
+home-manager switch --flake .#default
+echo "
+----------------------------------------
+----------- Script Finished ------------
+----- You can close this window now ----
+----------------------------------------
+"
---
+++ complementaryFiles/sysw.sh
@@ -0,0 +1,8 @@
+cd ~/.dotfiles/aierNix
+sudo nixos-rebuild switch --flake .#default
+echo "
+----------------------------------------
+----------- Script Finished ------------
+----- You can close this window now ----
+----------------------------------------
+"
