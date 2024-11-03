7f67b85217eacc98b10385d99161cabadd84cf03
aier <aierjjwu8@gmail.com>
2024-11-03 19:47:29 -0400

    Modifying default font size

--- Default/Home/HomeDconf/gnomeDesktopInterface.nix
+++ Default/Home/HomeDconf/gnomeDesktopInterface.nix
@@ -9,13 +9,13 @@ with lib.hm.gvariant;
       clock-format = "24h"; 
       color-scheme = "default"; # Prefer dark or light
       # cursor-theme = "LatteLightCursors"; # temporarily disabled
-      document-font-name = "Noto Serif CJK TC 13 @wght=400"; # Serif/Document font # Default pt 11
+      document-font-name = "Noto Serif CJK TC 12.5 @wght=400"; # Serif/Document font # Default pt 11
       enable-hot-corners = false;
       font-antialiasing = "rgba";
       font-hinting = "full";
-      font-name = "IBM Plex Sans 13"; # Sans/Interface/Backup font # Default pt 11
+      font-name = "IBM Plex Sans 12.5"; # Sans/Interface/Backup font # Default pt 11
       gtk-theme = "AdwGtk3";
-      monospace-font-name = "IBM Plex Mono 13"; # Mono font # default 10
+      monospace-font-name = "IBM Plex Mono 12"; # Mono font # default 10
       show-battery-percentage = true;
       text-scaling-factor = 1; 
     };
