38f9c847261f5ed1b2f6f3d5c68d115a655701e2
aier <aierjjwu8@gmail.com>
2024-11-03 17:33:13 -0400

    Enabling fractional scaling

--- Default/Home/HomeDconf/gnomeMutter.nix
+++ Default/Home/HomeDconf/gnomeMutter.nix
@@ -5,15 +5,19 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+
     "org/gnome/mutter" = {
+
       attach-modal-dialogs = true;
       center-new-windows = true;
       dynamic-workspaces = true; 
       edge-tiling = true; 
+      experimental-features = ["scale-monitor-framebuffer" "xwayland-native-scaling"];
       workspaces-only-on-primary = true; 
     };
 
     "org/gnome/mutter/keybindings" = {
+
       toggle-tiled-left = [ "<Super>left" ]; # Incompatible with PaperWM
       toggle-tiled-right = [ "<Super>right" ]; # Incompatible with PaperWM
     };
