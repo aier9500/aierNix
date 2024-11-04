0b7b6a79646c65ed3f30c8a64c228a74d8abf40b
aier <aierjjwu8@gmail.com>
2024-11-03 20:41:19 -0400

    + easyeffects-preset-selector + search-light

--- Default/Home/homeApps.nix
+++ Default/Home/homeApps.nix
@@ -34,9 +34,11 @@
     clipboard-indicator
     dash-to-dock
     disable-unredirect-fullscreen-windows # Not needed for PaperWM
+    easyeffects-preset-selector
     hide-top-bar # Incompatible with PaperWM
     launch-new-instance
     # paperwm
+    search-light
     quick-settings-audio-panel
     # rounded-window-corners-reborn # Incompatible with PaperWM
     unite
