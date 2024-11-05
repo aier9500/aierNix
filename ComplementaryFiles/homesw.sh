c570564f42a88da420b70d6650217c0bada96fc5
aier <aierjjwu8@gmail.com>
2024-11-05 19:28:24 -0400

    - rebuild bash files (declared in Home Manager)

--- ComplementaryFiles/homesw.sh
+++
@@ -1,8 +0,0 @@
-cd ~/.dotfiles/aierNix
-home-manager switch --flake .#default
-echo "
-----------------------------------------
------------ Script Finished ------------
------ You can close this window now ----
-----------------------------------------
-"
--- ComplementaryFiles/sysw.sh
+++
@@ -1,8 +0,0 @@
-cd ~/.dotfiles/aierNix
-sudo nixos-rebuild switch --flake .#default
-echo "
-----------------------------------------
------------ Script Finished ------------
------ You can close this window now ----
-----------------------------------------
-"
