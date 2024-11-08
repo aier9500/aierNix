8faf03952c4135985299bc68b0f985c665b96e38
aier <aierjjwu8@gmail.com>
2024-11-08 18:23:53 -0400

    Adding features to navigation layer

--- Default/System/SystemModules/systemKanata.nix
+++ Default/System/SystemModules/systemKanata.nix
@@ -27,7 +27,7 @@
         XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
         XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
         XX    XX   XX   XX   XX   XX   XX   @clt up   @crt XX   XX   XX   
-        XX     XX   XX   XX   XX   XX   XX   lft  down rght XX   XX   XX
+        XX     lsft XX   XX   XX   XX   XX   lft  down rght XX   XX   XX
         XX       lctl XX   XX   XX   XX   XX   XX   home end  XX   XX
         XX   XX   XX             XX             XX   XX   XX 
       )
